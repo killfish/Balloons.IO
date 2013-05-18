08eca5872267566658816b176417ad69ba45e22b
jason.goodwin <jgoodwin@mdialog.com>
2013-05-18 18:10:19 -0400

    Clean up the api service for save

--- services/kata-api-service.js
+++ services/kata-api-service.js
@@ -16,46 +16,7 @@ var mongoose = require('mongoose')
 //}
 
 exports.save = function(kata){
-  console.log(kata)
-  var kataObj = new Kata({
-    id: kata.id,
-    language: kata.language,
-    level: kata.level,
-    challengeTitle: kata.challengeTitle,
-    challengeCopy: kata.challengeCopy,
-    templateCode: kata.templateCode,
-    testCases: kata.testCases
-  })
-
+  var kataObj = new Kata(kata)
   console.log(kataObj)
-
   kataObj.save()
 }
-
-
-
-
-/*
- var mongoose = require('mongoose')
- , Schema = mongoose.Schema;
-
- var KataSchema = new Schema({
- id: String,
- language: String,
- level: String,
- challengeTitle: String,
- challengeCopy: String,
- templateCode: String,
- testCases: [
- {test: String, expectedResult: String}
- ],
- comments: [
- { body: String, date: Date }
- ],
- meta: {
- favs: Number
- }
- });
-
- mongoose.model('KataSchema', KataSchema)
-  */
